881af9c533dfd94c48fe4763300f6220968af3fe
Kadirhan54 <kadirhansaglam5454@gmail.com>
2023-10-16 14:18:13 +0300

    Add project files.

---
+++ YetGenAkbankJump.MVCClient/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OpenAI.Interfaces;
+using OpenAI.ObjectModels.RequestModels;
+using OpenAI.ObjectModels;
+using System.Diagnostics;
+using YetGenAkbankJump.MVCClient.Models;
+
+namespace YetGenAkbankJump.MVCClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IOpenAIService _openAiService;
+
+
+        public HomeController(ILogger<HomeController> logger, IOpenAIService openAiService)
+        {
+            _logger = logger;
+            _openAiService = openAiService;
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+
+            var imageResult = await _openAiService.Image.CreateImage(new ImageCreateRequest
+            {
+                Prompt = "A computer have legs and runs to the sea",
+                N = 3,
+                Size = StaticValues.ImageStatics.Size.Size512,
+                ResponseFormat = StaticValues.ImageStatics.ResponseFormat.Url,
+                User = "Kadirhan"
+            });
+
+            List<string> urls;
+
+            if (imageResult.Successful)
+            {
+                urls = imageResult.Results.Select(r => r.Url).ToList();
+            }
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
